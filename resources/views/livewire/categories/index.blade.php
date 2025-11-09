6bf84a06d1fc6e75683f1a3029ab82a31ea76206
Zachran Razendra <zachranraze@recodex.id>
2025-11-09 22:39:21 +0000

    change icon

--- resources/views/livewire/categories/index.blade.php
+++ resources/views/livewire/categories/index.blade.php
@@ -244,7 +244,7 @@ new class extends Component {
                                     variant="ghost"
                                     wire:click="toggleStatus({{ $category->id }})"
                                     title="Toggle Status"
-                                    icon="{{ $category->is_active ? 'eye' : 'eye-off' }}"
+                                    icon="{{ $category->is_active ? 'eye' : 'eye-slash' }}"
                                 />
 
                                 <flux:button
